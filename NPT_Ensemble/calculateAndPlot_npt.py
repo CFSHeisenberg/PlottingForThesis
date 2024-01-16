d44851fe61452fafa97b206444d28eb7918911eb
Michael Fill <m.fill@student.uibk.ac.at>
2024-01-16 13:54:03 +0100

    Added subfolder for NPT calculations, use NVT source code as framework

---
+++ NPT_Ensemble/calculateAndPlot_npt.py
@@ -0,0 +1,170 @@
+from concurrent import futures
+import os
+from tkinter import Entry
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+class DistancePlotter:
+    def __init__(self, merged_df_values, numAtoms, centroid_indices_j, guest_indices, numOfCentroids, lattice_values):
+        self.merged_df_values = merged_df_values
+        self.numAtoms = numAtoms
+        self.centroid_indices_j = centroid_indices_j
+        self.guest_indices = guest_indices
+        self.numOfCentroids = numOfCentroids
+        self.lattice_values = lattice_values
+
+    def imaging(self, coords, previous_coords, i):
+        difference = np.array(coords - previous_coords)
+        difference_new = difference - np.round(np.divide(difference, self.lattice_values).astype(float)) * self.lattice_values 
+        coords_changed = previous_coords + difference_new
+        return coords_changed
+
+    def calculate_distance(self, i, selected_centroids):
+        if i == 1:  
+            # Get partial numpy array of current step. Takes only the rows of the current step
+            stepframe_values = self.merged_df_values[(i-1)*self.numAtoms:i*self.numAtoms, :]
+
+            # Get partial numpy array of shape (108,7) only containing those entries of stepframe_values whose fifth element, i.e. the cleanedIndex are in centroid_indices_j
+            stepframe_j_values = stepframe_values[np.isin(stepframe_values[:, 5], self.centroid_indices_j)]
+            #print(self.centroid_indices_j)
+
+            # Get coordinates of the atoms making up centroids in current step
+            centroid_coords = stepframe_j_values[:, 1:4]
+            #print(centroid_coords)
+
+            # Get partial numpy array of shape (6, 7) only containing those entries of stepframe_values whose fifth element, i.e. the cleanedIndex are in guest_indices
+            stepframe_g_values = stepframe_values[np.isin(stepframe_values[:, 5], self.guest_indices)]
+
+            # Get coordinates of the relevant guest atoms in current step
+            guest_coords = stepframe_g_values[:, 1:4]
+            #print(guest_coords)
+
+            # Get coordinates of cartesian centroids for both host and guest via the mean of their coordinates
+            center_guest_coords = np.mean(guest_coords, axis=0)
+            center_host_coords = np.mean(np.split(centroid_coords, self.numOfCentroids), axis=1)
+
+            # Calculate distance between each centroid to the guest centroid
+            distance_to_center = np.linalg.norm((center_guest_coords - center_host_coords).astype(float), axis=1)
+
+        else:
+            # Get partial numpy array of current step. Takes only the rows of the current step
+            stepframe_values = self.merged_df_values[(i-1)*self.numAtoms:i*self.numAtoms, :]
+
+            # Get partial numpy array of previous step. Takes only the rows of the previous step
+            stepframe_values_previous = self.merged_df_values[(i-2)*self.numAtoms:(i-1)*self.numAtoms, :]
+
+            # Get partial numpy array of shape (108,7) of the current step only containing those entries of stepframe_values whose fifth element, i.e. the cleanedIndex are in centroid_indices_j
+            stepframe_j_values = stepframe_values[np.isin(stepframe_values[:, 5], self.centroid_indices_j)]
+
+            # Get partial numpy array of shape (108,7) of the previous step only containing those entries of stepframe_values_next whose fifth element, i.e. the cleanedIndex are in centroid_indices_j
+            stepframe_j_values_previous = stepframe_values_previous[np.isin(stepframe_values_previous[:, 5], self.centroid_indices_j)]       
+
+            # Get coordinates of the atoms making up centroids in current step
+            centroid_coords = stepframe_j_values[:, 1:4]
+            # Get coordinates of the atoms making up centroids in previous step
+            centroid_coords_previous = stepframe_j_values_previous[:, 1:4]
+
+            # Get partial numpy array of shape (6, 7) only containing those entries of stepframe_values whose fifth element, i.e. the cleanedIndex are in guest_indices
+            stepframe_g_values = stepframe_values[np.isin(stepframe_values[:, 5], self.guest_indices)]
+
+            # Get coordinates of the relevant guest atoms in current step
+            guest_coords = stepframe_g_values[:, 1:4]
+
+            # Compare centroid coordinates in next step to centroid coordinates in current step, image them if necessary
+            centroid_coords_imaged = self.imaging(centroid_coords, centroid_coords_previous, i)
+
+            # Overwrite the values in the dataframe accessed by stepframe_j_values with centroid_coords_imaged for the next step
+            self.merged_df_values[(i-1)*self.numAtoms:i*self.numAtoms, 1:4][np.isin(stepframe_values[:,5], self.centroid_indices_j)] = centroid_coords_imaged
+
+            # Get coordinates of cartesian centroids for both host and guest centroids via the mean of their coordinates
+            center_guest_coords = np.mean(guest_coords, axis=0)
+            center_host_coords_imaged = np.mean(np.split(centroid_coords_imaged, self.numOfCentroids), axis=1)
+
+            # Calculate distance between each centroid to the guest centroid
+            distance_to_center = np.linalg.norm((center_guest_coords - center_host_coords_imaged).astype(float), axis=1)
+
+        # Return the calculated distances for selected centroids
+        return [distance_to_center[j-1] for j in selected_centroids]
+    
+    def calcAndSaveAllDistancesForNSteps(self, stepsToCalculate, directory):
+        filename = 'distances_for_' + str(stepsToCalculate) + '_steps.npy'
+        # Get the total steps to plot from the entry field
+        try:
+        # Use ThreadPoolExecutor to parallelize the distance calculation
+            with futures.ThreadPoolExecutor(1) as executor:
+                # List to store the future objects
+                futures_list = []
+
+                # Loop over the specified steps
+                for i in range(1, stepsToCalculate + 1):
+                    # Submit the distance calculation task to the executor
+                    future = executor.submit(self.calculate_distance, i, np.arange(1, self.numOfCentroids + 1))
+                    futures_list.append(future)
+
+                # Wait for all the tasks to complete and retrieve the results
+                results = [future.result() for future in futures_list]
+                savepath = os.path.join(directory, filename)
+                np.save(savepath, results)
+                return savepath
+        except ValueError:
+            print("Invalid input. Please enter a valid integer for the total steps.")
+                
+
+    def plot_distances(self, stepsToPlot, centroid_vars):
+        # Get the total steps to plot from the entry field
+        total_steps = stepsToPlot
+        try:
+            # Convert the total steps to an integer
+            total_steps = int(total_steps)
+
+            # Get the selected centroids to plot
+            selected_centroids = [var.get() for var in centroid_vars]
+            selected_centroids = [i+1 for i, val in enumerate(selected_centroids) if val == 1]
+
+            # Use ThreadPoolExecutor to parallelize the distance calculation
+            with futures.ThreadPoolExecutor(1) as executor:
+                # List to store the future objects
+                futures_list = []
+
+                # Loop over the specified steps
+                for i in range(1, total_steps + 1):
+                    # Submit the distance calculation task to the executor
+                    future = executor.submit(self.calculate_distance, i, selected_centroids)
+                    futures_list.append(future)
+
+                # Wait for all the tasks to complete and retrieve the results
+                results = [future.result() for future in futures_list]
+
+            # Plot the distances for each selected centroid to the guest centroid
+            for j, centroid in enumerate(selected_centroids):
+                plt.plot(range(1, total_steps + 1), [result[j] for result in results], label=f'Centroid {centroid}')
+
+            # Edit and show plot
+            plt.xlabel('Step Number')
+            plt.ylabel('Distance to Guest Centroid [Å]')
+            plt.legend()
+            plt.show()
+        except ValueError:
+            print("Invalid input. Please enter a valid integer for the total steps.")
+            
+def plotFromSavedDistances(centroid_vars, file_path):
+        
+    #Open numpy array from file
+    results = np.load(file_path)
+        
+    #Get the steps to plot from the name of the file
+    stepsToPlot = int(file_path.split('_')[2])
+        
+    # Get the selected centroids to plot
+    selected_centroids = [var.get() for var in centroid_vars]
+    selected_centroids = [i+1 for i, val in enumerate(selected_centroids) if val == 1]
+            
+    # Plot the distances for each selected centroid to the guest centroid
+    for j, centroid in enumerate(selected_centroids):            plt.plot(range(1, stepsToPlot + 1), [result[j] for result in results], label=f'Centroid {centroid}')
+
+    # Edit and show plot
+    plt.xlabel('Step Number')
+    plt.ylabel('Distance to Guest Centroid [Å]')
+    plt.legend()
+    plt.show()
---
+++ NPT_Ensemble/main_npt.py
@@ -0,0 +1,295 @@
+import tkinter as tk
+from tkinter import filedialog
+import os.path
+from readerAndLoader import ReaderAndLoader
+from calculateAndPlot import DistancePlotter
+from calculateAndPlot import plotFromSavedDistances
+import tkinter as tk
+from tkinter import *
+import os
+
+DEFAULT_FILE_PREFIX = "MIL68Ga"
+DEFAULT_DIRECTORY = "/home/mfi/Desktop/mfi/MIL-68Ga-guest"
+DEFAULT_INDEX_FILENAME = "indicessorted.dat"  # Set default index filename
+
+ReaderAndLoader = ReaderAndLoader()
+distancePlotter = None
+centroid_vars = []
+
+def read_and_save_data():
+    directory = directory_entry.get()
+    file_prefix = prefix_entry.get()
+    index_filename = index_entry.get()
+    ReaderAndLoader.read_and_save_data(directory, file_prefix, index_filename)
+    load_button.config(bg="green")  # Change button color to green
+    check_saved_data()
+    #plot_button.config(state=tk.NORMAL)  # Enable the plot button
+
+def browse_directory():
+    directory = filedialog.askdirectory()
+    directory_entry.delete(0, tk.END)
+    directory_entry.insert(tk.END, directory)
+    check_saved_data()
+
+def browse_distances_file():
+    file_path = filedialog.askopenfilename()
+    distances_entry.delete(0, tk.END)
+    distances_entry.insert(tk.END, file_path)
+    check_saved_data_distances()
+    #plotfromdistance_button.config(state=tk.NORMAL)  # Enable the plot button
+
+def check_saved_data_distances():
+    file_path = distances_entry.get()
+    if os.path.isfile(file_path):
+        saved_distance_data_label.config(text="Saved distances found!", fg="green")
+        plotfromdistance_button.config(state=tk.NORMAL)  # Enable the plot button
+        directory = directory_entry.get()
+        index_filename = index_entry.get()
+        
+        data, numAtoms, centroid_indices_flat, guest_indices, numOfCentroids, lattice_values, centroid_indices = ReaderAndLoader.load_data(directory, index_filename, file_path)  # Call the function to load the saved data    
+
+       
+            
+        # Create tick boxes for each centroid
+        global centroid_vars
+        centroid_vars = []
+        for i in range(numOfCentroids):
+            var = tk.BooleanVar()
+            centroid_vars.append(var)
+            centroid_checkbox = tk.Checkbutton(window, text=f"Centroid {i+1}", variable=var)
+            centroid_checkbox.pack()
+                    
+    else:
+        saved_distance_data_label.config(text="No saved distances found.", fg="black")
+        plotfromdistance_button.config(state=tk.DISABLED)  # Disable the plot button
+
+def check_saved_data():
+    file_path = os.path.join(directory_entry.get(), "sourcedata.npy")
+    if os.path.isfile(file_path):
+        load_saved_data_button.config(state=tk.NORMAL)
+        saved_data_label.config(text="Saved data found in chosen directory!", fg="green")
+        plot_button.config(state=tk.DISABLED)  # Disable the plot button
+
+    else:
+        load_saved_data_button.config(state=tk.DISABLED)
+        saved_data_label.config(text="No saved data found in chosen directory.", fg="black")
+        plot_button.config(state=tk.DISABLED)  # Disable the plot button
+
+def plot_distances():
+    global distancePlotter
+    global centroid_vars
+    #data, numAtoms, centroid_indices_flat, guest_indices, numOfCentroids, lattice_values = ReaderAndLoader.load_data(directory_entry.get(), index_entry.get(), os.path.join(directory_entry.get(), "sourcedata.npy"))
+    #distancePlotter = DistancePlotter(merged_df_values=data, numAtoms=numAtoms, centroid_indices_j=centroid_indices_flat, guest_indices=guest_indices, numOfCentroids=numOfCentroids, lattice_values=lattice_values)
+    stepsToPlot = int(stepsToPlot_entry.get())
+    print("centroid vards=" ,centroid_vars)
+    distancePlotter.plot_distances(stepsToPlot, centroid_vars)
+    
+    # Start the main event loop
+def load_saved_data():
+    global distancePlotter
+    directory = directory_entry.get()
+    index_filename = index_entry.get()
+    file_path = os.path.join(directory_entry.get(), "sourcedata.npy")
+    
+    data, numAtoms, centroid_indices_flat, guest_indices, numOfCentroids, lattice_values, centroid_indices = ReaderAndLoader.load_data(directory, index_filename, file_path)  # Call the function to load the saved data
+    distancePlotter = DistancePlotter(merged_df_values=data, numAtoms=numAtoms, centroid_indices_j=centroid_indices, guest_indices=guest_indices, numOfCentroids=numOfCentroids, lattice_values=lattice_values)
+    
+    num_atoms_label.config(text=f"Number of Atoms: {numAtoms}")
+    num_centroids_label.config(text=f"Number of Centroids: {numOfCentroids}")
+    lattice_params_label.config(text=f"Lattice Parameters: {lattice_values}")
+    plot_button.config(state=tk.NORMAL)  # Enable the plot button
+
+    # Create tick boxes for each centroid
+    global centroid_vars
+    centroid_vars = []
+    for i in range(numOfCentroids):
+        var = tk.BooleanVar()
+        centroid_vars.append(var)
+        centroid_checkbox = tk.Checkbutton(window, text=f"Centroid {i+1}", variable=var)
+        centroid_checkbox.pack()
+        
+# Function to ask for directory
+def ask_directory():
+    directory = filedialog.askdirectory()
+    if directory:
+        calculate_distances(directory)
+        
+def plot_from_distances_file():
+    file_path_distance = distances_entry.get()
+    index_filename = index_entry.get()
+    file_path = os.path.join(directory_entry.get(), "sourcedata.npy")
+        
+    plotFromSavedDistances(centroid_vars, file_path_distance)
+
+# Function to calculate distances and save data
+def calculate_distances(save_directory):
+    
+    steps = int(stepsToCalc_entry.get())
+    index_filename = index_entry.get()
+    directory = directory_entry.get()
+    file_path = os.path.join(directory_entry.get(), "sourcedata.npy")
+    
+    data, numAtoms, centroid_indices_flat, guest_indices, numOfCentroids, lattice_values, centroid_indices = ReaderAndLoader.load_data(directory, index_filename, file_path)  # Call the function to load the saved data
+    distancePlotter = DistancePlotter(merged_df_values=data, numAtoms=numAtoms, centroid_indices_j=centroid_indices, guest_indices=guest_indices, numOfCentroids=numOfCentroids, lattice_values=lattice_values)
+    file_path = distancePlotter.calcAndSaveAllDistancesForNSteps(steps, save_directory)
+    saved_data_label.config(text=f"Distances saved to: {file_path}")
+    
+
+# Create the main window
+window = tk.Tk()
+window.title("Trajectory Analyzer for centroid distances")
+
+scroll_bar = Scrollbar(window)
+
+scroll_bar.pack(side=RIGHT, fill=Y)
+
+# Create a frame for the directory, prefix, and index filename widgets
+input_frame = tk.Frame(window, highlightbackground="red", highlightthickness=4)
+input_frame.pack(pady=20)
+
+# Create a label for the frame
+label = tk.Label(input_frame, text="MANDATORY: File locations", fg="red")
+label.pack()
+
+# Create the directory label and entry
+directory_label = tk.Label(input_frame, text="Directory containing XYZ files and indices file:")
+directory_label.pack()
+directory_entry = tk.Entry(input_frame)
+directory_entry.insert(tk.END, DEFAULT_DIRECTORY)  # Set default directory
+directory_entry.pack()
+directory_entry.config(highlightbackground="red", highlightthickness=1)  # Add red rectangle
+
+# Create the browse button
+browse_button = tk.Button(input_frame, text="Browse", command=browse_directory)
+browse_button.pack()
+
+# Create the prefix label and entry
+prefix_label = tk.Label(input_frame, text="File Prefix:")
+prefix_label.pack()
+prefix_entry = tk.Entry(input_frame)
+prefix_entry.insert(tk.END, DEFAULT_FILE_PREFIX)  # Set default file prefix
+prefix_entry.pack()
+prefix_entry.config(highlightbackground="red", highlightthickness=1)  # Add red rectangle
+
+# Create the index filename label and entry
+index_label = tk.Label(input_frame, text="Index Filename:")
+index_label.pack()
+index_entry = tk.Entry(input_frame)
+index_entry.insert(tk.END, DEFAULT_INDEX_FILENAME)  # Set default index filename
+index_entry.pack()
+index_entry.config(highlightbackground="red", highlightthickness=1)  # Add red rectangle
+
+
+
+# Create a frame for the load and load saved data buttons
+button_frame = tk.Frame(window, highlightbackground="blue", highlightthickness=4)
+button_frame.pack(pady=20)
+
+# Create a label for the frame
+button_label = tk.Label(button_frame, text="Read and save trajectories or load existing data", fg="blue")
+button_label.pack()
+
+# Create the load button
+load_button = tk.Button(button_frame, text="Read and save XYZ Files", command=read_and_save_data)
+load_button.pack()
+
+# Create the load saved data button
+load_saved_data_button = tk.Button(button_frame, text="Load Saved Data", command=load_saved_data, state=tk.DISABLED)
+load_saved_data_button.pack()
+
+# Create the saved data label
+saved_data_label = tk.Label(button_frame, text="", fg="black")
+saved_data_label.pack()
+
+# Create the saved data label
+saved_data_label = tk.Label(button_frame, text="", fg="black")
+saved_data_label.pack()
+
+
+# Create the stepstoplot entry
+stepsToPlot_label = tk.Label(button_frame, text="Steps to plot:")
+stepsToPlot_label.pack()
+stepsToPlot_entry = tk.Entry(button_frame)
+stepsToPlot_entry.pack()
+
+# Create the plot button
+plot_button = tk.Button(button_frame, text="Plot from loaded data", state=tk.DISABLED, command = plot_distances)
+plot_button.pack()
+
+
+
+# Create a new frame for the stepstocalc entry and calculate distances button
+calc_frame = tk.Frame(window, highlightbackground="green", highlightthickness=4)
+calc_frame.pack(pady=20)
+
+# Create a label for the frame
+calc_label = tk.Label(calc_frame, text="Calculate n steps and save results to file for later plotting", fg="green")
+calc_label.pack()
+
+
+# Create the stepstocalc entry
+stepsToCalc_label = tk.Label(calc_frame, text="Steps to calc:")
+stepsToCalc_label.pack()
+stepsToCalc_entry = tk.Entry(calc_frame)
+stepsToCalc_entry.pack()
+
+# Create the calculate distances for steps button
+calculate_distances_button = tk.Button(calc_frame, text="Calculate Distances for Steps", command=lambda: ask_directory())
+calculate_distances_button.pack()
+
+
+# Create a frame for the load precalculated distance file and plot chosen centroids section
+load_distances_frame = tk.Frame(window, highlightbackground="orange", highlightthickness=4)
+load_distances_frame.pack(pady=20)
+
+# Create a label for the frame
+load_distances_label = tk.Label(load_distances_frame, text="Load precalculated distance file and plot chosen centroids", fg="orange")
+load_distances_label.pack()
+
+# Create second browse button for distances file
+browse_distances_button = tk.Button(load_distances_frame, text="Browse file containing distances", command=browse_distances_file)
+browse_distances_button.pack()
+
+# Create the distances file label and entry
+distances_label = tk.Label(load_distances_frame, text="Distances File:")
+distances_label.pack()
+distances_entry = tk.Entry(load_distances_frame)
+distances_entry.pack()
+
+# Create the saved distance data label
+saved_distance_data_label = tk.Label(load_distances_frame, text="", fg="black")
+saved_distance_data_label.pack()
+
+# Create the plotfromdistancefile button
+plotfromdistance_button = tk.Button(load_distances_frame, text="Plot from loaded distances", state=tk.DISABLED, command=plot_from_distances_file)
+plotfromdistance_button.pack()
+
+
+
+# Create a frame for the properties of simulation
+properties_frame = tk.Frame(window, highlightbackground="purple", highlightthickness=4)
+properties_frame.pack(pady=20)
+
+# Create a label for the frame
+properties_label = tk.Label(properties_frame, text="Properties of simulation", fg="purple")
+properties_label.pack()
+
+# Create the labels for numAtoms, number of centroids and lattice parameters
+num_atoms_label = tk.Label(properties_frame, text="Number of Atoms: ")
+num_atoms_label.pack()
+
+num_centroids_label = tk.Label(properties_frame, text="Number of Centroids: ")
+num_centroids_label.pack()
+
+lattice_params_label = tk.Label(properties_frame, text="Lattice Parameters: ")
+lattice_params_label.pack()
+
+
+
+# Check if the default directory already contains the "sourcedata" file
+check_saved_data()
+
+# Create the main loop
+window.mainloop()
+
+window.mainloop()
